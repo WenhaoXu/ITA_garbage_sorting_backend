f2546791bdd26685113dda273b1efa3a14cbfb95
XUHO3 <howells.xu@oocl.com>
2019-11-16 14:07:23 +0800

    init

---
+++ src/main/java/com/ita/garbage_sorting/GarbageSortingApplication.java
@@ -0,0 +1,13 @@
+package com.ita.garbage_sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarbageSortingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ita/garbage_sorting/GarbageSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita.garbage_sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
